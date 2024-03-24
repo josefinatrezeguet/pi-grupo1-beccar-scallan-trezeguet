fb6fa7f36d8c3425b4ab85fb25f62916fb1feb05
rociobeccarv <rbeccarvarela@udesa.edu.ar>
2024-03-24 12:00:44 -0300

    test

--- ProyectoIntegrador/routes/product.js
+++ ProyectoIntegrador/routes/product.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let autos = require('../db/index.js');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res) {
+  return res.send(autos.lista);
 });
 
+router.get('/color/:color', function(req, res) {
+  let color = req.params.color;
+  let resultadoBusqueda = [];
+
+  for (let i = 0; i < autos.lista.length; i++) {
+    if (autos.lista[i].color === color.toLowerCase()) {
+      resultadoBusqueda.push(autos.lista[i]);
+    }
+  }
+
+  if (resultadoBusqueda.length > 0) {
+    return res.send(resultadoBusqueda);
+  } else {
+    return res.send(`No hay autos de color ${color}.`);
+  }
+});
+
+// Ruta para traer productos dependiendo de lo que busquen //
 module.exports = router;
