c476fd1bfbf0aa9a82559164040b46f96b9ef8c5
catascallan <cscallan@udesa.edu.ar>
2024-03-24 14:26:07 -0300

    Añadiendo rutas

--- ProyectoIntegrador/routes/product.js
+++ ProyectoIntegrador/routes/product.js
@@ -1,27 +1,13 @@
-let express = require('express');
-let router = express.Router();
-let autos = require('../db/index.js');
+const express = require('express');
+const router = express.Router();
+let product = require('../db/index.js');
 
+//Ruta raíz de los productos (inicio)
 router.get('/', function(req, res) {
-  return res.send(autos.lista);
 });
 
-router.get('/color/:color', function(req, res) {
-  let color = req.params.color;
-  let resultadoBusqueda = [];
-
-  for (let i = 0; i < autos.lista.length; i++) {
-    if (autos.lista[i].color === color.toLowerCase()) {
-      resultadoBusqueda.push(autos.lista[i]);
-    }
-  }
-
-  if (resultadoBusqueda.length > 0) {
-    return res.send(resultadoBusqueda);
-  } else {
-    return res.send(`No hay autos de color ${color}.`);
-  }
+//Ruta detalle de los productos
+router.get('/detalle/:id', function(req, res) {
 });
 
-// Ruta para traer productos dependiendo de lo que busquen //
 module.exports = router;
