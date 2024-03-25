180cc36f6f89c4348bf4640de59457d6e88172e4
catascallan <cscallan@udesa.edu.ar>
2024-03-24 22:48:38 -0300

    Migrando a archivos . ejs

--- ProyectoIntegrador/app.js
+++ ProyectoIntegrador/app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const productRouter = require("./routes/product")
 
 var app = express();
 
@@ -22,7 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/product", productRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
