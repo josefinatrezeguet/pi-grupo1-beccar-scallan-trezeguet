4eb0d4d480c3085271145515b602b715c82e24aa
rociobeccarv <rbeccarvarela@udesa.edu.ar>
2024-04-04 13:03:29 -0300

    test

--- ProyectoIntegrador/Controllers/indexControllers.js
+++ ProyectoIntegrador/Controllers/indexControllers.js
@@ -1,7 +1,47 @@
 let express = require("express");
 let router = express.Router();
-let index = require("../db/")
+let maquillaje = require("../db/")
 
-let controllers = {
-
+let controller = {
+    marca: function(req,res){
+        let datosMarca = function(marca)
+        {
+            let newMaquillaje = []
+            for (i=0; i<maquillaje.length; i++){
+                if (maquillaje[i].marca == marca){
+                    newMaquillaje.push(maquillaje[i])
+                }
+            }
+            return newMaquillaje
+        }
+        return res.render("index",{"maquillaje": datosMarca(req.params.mark)})
+    },
+    producto: function(req,res){
+        let datosMarca = function(producto)
+        {
+            let newMaquillaje = []
+            for (i=0; i<maquillaje.length; i++){
+                if (maquillaje[i].producto == producto){
+                    newMaquillaje.push(maquillaje[i])
+                }
+            }
+            return newMaquillaje
+        }
+        return res.render("index",{"maquillaje": datosMarca(req.params.producto)})
+    },
+    color: function(req,res){
+        let datosMarca = function(color)
+        {
+            let newMaquillaje = []
+            for (i=0; i<maquillaje.length; i++){
+                if (maquillaje[i].color == color){
+                    newMaquillaje.push(autos[i])
+                }
+            }
+            return newMaquillaje
+        }
+        return res.render("index",{"maquillaje": datosMarca(req.params.color)})
+    }
 }
+
+module.exports = controller;
