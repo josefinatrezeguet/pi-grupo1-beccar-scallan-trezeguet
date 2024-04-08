b801d3b47c80621b199efcde4b8106fd2bd89ed5
catascallan <cscallan@udesa.edu.ar>
2024-04-07 22:46:10 -0300

    comienzo archivo js en /db

---
+++ ProyectoIntegrador/db/index.js
@@ -0,0 +1,347 @@
+const db = {
+    usuario: {
+        email:"emiliamernes@gmail.com",
+        usuario: "emimernes",  
+        contrasenia: "Duki123",
+        nacimiento: "29/10/1996",
+        dni: "46442824",
+        fotoPerfil: ""
+    },
+
+    productos: [
+        {
+            id: 1,
+            imagen: "images/products/rare.png",
+            nombre: "",
+            descripcion: "",
+            comentarios: [
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                } 
+            ]
+        },
+ 
+        {
+            id : 2,
+            imagen: "images/products/sky.png",
+            nombre: "",
+            descripcion: "",
+            comentarios: [
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                } 
+            ]
+        },
+        {
+            id : 3,
+            imagen: "images/products/lifter.png",
+            nombre: "",
+            descripcion: "",
+            comentarios: [
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                } 
+            ]
+        },
+        {
+            id : 4,
+            imagen: "images/products/toofaced.png",
+            nombre: "",
+            descripcion: "",
+            comentarios: [
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                } 
+            ]
+        },
+        {
+            id : 5,
+            imagen: "images/products/huda.png",
+            nombre: "",
+            descripcion: "",
+            comentarios: [
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                } 
+            ]
+        },
+        {
+            id: 6,
+            imagen: "images/products/ofra.png",
+            nombre: "",
+            descripcion: "",
+            comentarios: [
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                } 
+            ]
+        },
+        {
+            id: 7,
+            imagen: "images/products/nyx.png",
+            nombre: "",
+            descripcion: "",
+            comentarios: [
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                } 
+            ]
+        },
+        {
+            id: 8,
+            imagen: "images/products/mac.png",
+            nombre: "",
+            descripcion: "",
+            comentarios: [
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                } 
+            ]
+        },
+        {
+            id: 9,
+            imagen: "",
+            nombre: "",
+            descripcion: "",
+            comentarios: [
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                } 
+            ]
+        },
+        {
+            id: 10,
+            imagen: "",
+            nombre: "",
+            descripcion: "",
+            comentarios: [
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                }, 
+                {
+                    usuario: "",
+                    coment: "",
+                    foto: "",
+                } 
+            ]
+        },
+    ],    
+};
+
+
+module.exports = db;
