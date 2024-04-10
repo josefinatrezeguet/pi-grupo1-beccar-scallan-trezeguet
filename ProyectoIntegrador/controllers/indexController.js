5543c89f3a2d8aecb3cfda1e710158d6b566ef05
catascallan <cscallan@udesa.edu.ar>
2024-04-10 19:10:14 -0300

    .

--- ProyectoIntegrador/controllers/indexController.js
+++ ProyectoIntegrador/controllers/indexController.js
@@ -2,7 +2,7 @@ const db = require('../db/index');
 
 const indexController = {
     index: function(req, res) {
-        res.render('index', {title: "Aura Beauty - HOME", productos: db.productos});
+        res.render('index', {title: "Aura Beauty", productos: db.productos});
     },
     search: function(req, res) {
         res.render('search-results', {title: "Resultados de búsqueda"});
