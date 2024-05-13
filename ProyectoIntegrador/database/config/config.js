94fa8e9cfc48538cb078b8d0136e289e7259bb65
catascallan <cscallan@udesa.edu.ar>
2024-05-13 13:33:28 -0300

    .

--- ProyectoIntegrador/database/config/config.js
+++ ProyectoIntegrador/database/config/config.js
@@ -3,7 +3,7 @@ module.exports =
   "development": {
     "username": "root",
     "password": "root",
-    "database": "",
+    "database": "", 
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
