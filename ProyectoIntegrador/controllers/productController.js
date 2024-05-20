d68d0fe046e3481ea1709b8fa38ea8c2af0e1505
rociobeccarv <rbeccarvarela@udesa.edu.ar>
2024-05-20 16:58:16 -0300

    product controller

--- ProyectoIntegrador/controllers/productController.js
+++ ProyectoIntegrador/controllers/productController.js
@@ -2,10 +2,37 @@ const db = require('../database/models');
 
 const productController = {
     index: function(req, res) {
-        res.render('product', {title: "Detalle del producto", productos: null});
+
+        let id = req.params.id;
+
+        
+        let comentarios;
+        let productos;
+
+        db.Producto.findByPk(id)
+        .then(function(results){
+            productos = results;
+            return db.Comentario.findAll({
+                limit: 5,
+            });
+        })
+        .then(function(results){
+            comentarios = results;
+            return res.render('product', {title:"Productos", productos: productos, comentarios: comentarios});
+        })
+        .catch(function(error){
+            console.log(error);
+        });
     },
     create: function(req, res) {
-        res.render('product-add', {title: "Añadir producto", usuario: null});
+
+        db.Usuario.findOne()
+            .then(function(results){
+                return res.render('product-add', {title:"añadir producto", usuario: results});
+            })
+            .catch(function(error){
+                console.log(error);
+            });
     }
 }
 
