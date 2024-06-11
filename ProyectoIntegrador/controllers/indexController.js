d220b42bf8c1cb5c1d08d38e1a137f02e2241821
catascallan <cscallan@udesa.edu.ar>
2024-06-11 14:40:32 -0300

    modif en home page

--- ProyectoIntegrador/controllers/indexController.js
+++ ProyectoIntegrador/controllers/indexController.js
@@ -3,9 +3,15 @@ const { Op } = db.Sequelize;
 
 const indexController = {
     index: (req, res, next) => {
-        db.Producto.findAll({
+        const filtrado = {
             order: [['createdAt', 'DESC']],
-        })
+            include: [
+                { association: 'comentarios' },
+                { association: 'usuario' }
+            ]
+        };
+
+        db.Producto.findAll(filtrado)
         .then((productos) => {
             res.render('index', {
                 title: "Aura Beauty",
@@ -23,20 +29,27 @@ const indexController = {
 
     search: (req, res, next) => {
         const busqueda = req.query.search;
-        const filtro = {
+
+        const filtrado = {
             where: {
-                nombre: {
-                    [Op.like]: `%${busqueda}%`,
-                },
+                [Op.or]: [
+                    { nombre: { [Op.like]: `%${busqueda}%` } },
+                    { descripcion: { [Op.like]: `%${busqueda}%` } }
+                ]
             },
+            order: [['createdAt', 'DESC']],
+            include: [
+                { association: 'comentarios' },
+                { association: 'usuario' }
+            ]
         };
 
-        db.Producto.findAll(filtro)
+        db.Producto.findAll(filtrado)
         .then((resultados) => {
             res.render('search-results', {
                 title: "Resultados de búsqueda",
                 productos: resultados,
-                usuario: req.session.user,
+                usuario: req.session.user
             });
         })
         .catch((error) => {
