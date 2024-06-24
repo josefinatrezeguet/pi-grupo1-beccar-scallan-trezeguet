217971cb05108cc48ff5972fdd1ff322b32d8ecf
catascallan <cscallan@udesa.edu.ar>
2024-06-24 14:27:59 -0300

    detalles prolijidad de código

--- ProyectoIntegrador/routes/users.js
+++ ProyectoIntegrador/routes/users.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const usersController = require("../controllers/usersController");
 const { body } = require('express-validator');
 const db = require('../database/models');
